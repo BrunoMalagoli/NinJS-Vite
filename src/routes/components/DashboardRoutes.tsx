ded43da3526ffd39f7cefcf2d7556fe6dc5fa6f8
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-30 20:54:08 -0300

    Added DashboardRoutes for private routes

---
+++ src/routes/components/DashboardRoutes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../../pages/dashboard/DashboardPage';
+
+const DashboardRoutes = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<Dashboard />} />
+		</Routes>
+	);
+};
+
+export default DashboardRoutes;
