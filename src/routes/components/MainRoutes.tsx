97b7a5379f16c8d07236b394f23fd48c29824aef
Jalinson Diaz <zjaaaldev@gmail.com>
2022-08-30 20:54:21 -0300

    Changed to contain all public routes

--- src/routes/components/MainRoutes.tsx
+++ src/routes/components/MainRoutes.tsx
@@ -1,53 +1,17 @@
-import { FC } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { LoginPage } from '../../pages/auth/login/LoginPage'
-import { RegisterPage } from '../../pages/auth/register/RegisterPage'
-import Dashboard from '../../pages/dashdoard'
-import { Hero } from '../../pages/home'
-
-type PropsMiddlewaresAutentication = {
-  children: JSX.Element
-}
+import { FC } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import LoginPage from '../../pages/auth/login/LoginPage';
+import RegisterPage from '../../pages/auth/register/RegisterPage';
+import Dashboard from '../../pages/dashboard/DashboardPage';
+import { Hero } from '../../pages/home';
+import DashboardRoutes from './DashboardRoutes';
 
 export const MainRoutes = () => {
-  const UserLogged: FC<PropsMiddlewaresAutentication> = ({ children }) => {
-    if (localStorage.getItem('token'))
-      return <Navigate to="/dashboard" replace={true} />
-    return children
-  }
-  const RequireAuth: FC<PropsMiddlewaresAutentication> = ({ children }) => {
-    if (!localStorage.getItem('token'))
-      return <Navigate to="/login" replace={true} />
-    return children
-  }
-
-  return (
-    <Routes>
-      <Route path="/" element={<Hero />} />
-      <Route
-        path="/login"
-        element={
-          <UserLogged>
-            <LoginPage />
-          </UserLogged>
-        }
-      />
-      <Route
-        path="/register"
-        element={
-          <UserLogged>
-            <RegisterPage />
-          </UserLogged>
-        }
-      />
-      <Route
-        path="/dashboard"
-        element={
-          <RequireAuth>
-            <Dashboard />
-          </RequireAuth>
-        }
-      />
-    </Routes>
-  )
-}
+	return (
+		<Routes>
+			<Route path='/' element={<Hero />} />
+			<Route path='/login' element={<LoginPage />} />
+			<Route path='/register' element={<RegisterPage />} />
+		</Routes>
+	);
+};
