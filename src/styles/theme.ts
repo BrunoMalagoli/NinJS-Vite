da88d1052693e0d1ea6cde99af6f027a6800b51a
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-05 17:11:21 -0300

    added colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,8 @@ const theme = extendTheme({
 	colors: {
 		primaryYellow: '#F8D859',
 		primaryBG: '#16191C',
+		primaryBGLight: '#40474F',
+		primaryBGShade: '#090A0B',
 		primaryColor: '#F3F4F6',
 		secondaryColor: '#C1C1C1'
 	}
