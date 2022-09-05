c03729ca4f827b74931cccf156c72b2786ca7ba6
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-05 16:56:54 -0300

    Added Leaderboard and settings routes

--- src/routes/components/DashboardRoutes.tsx
+++ src/routes/components/DashboardRoutes.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from '../../pages/dashboard/DashboardPage';
+import LeaderboardPage from '../../pages/leaderboard/LeaderboardPage';
+import SettingsPage from '../../pages/settings/SettingsPage';
 
 const DashboardRoutes = () => {
 	return (
-		<Routes>
-			<Route path='/' element={<Dashboard />} />
-		</Routes>
+		<>
+			<Routes>
+				<Route path='/' element={<Dashboard />} />
+				<Route path='/settings' element={<SettingsPage />} />
+				<Route path='/leaderboard' element={<LeaderboardPage />} />
+			</Routes>
+		</>
 	);
 };
 
