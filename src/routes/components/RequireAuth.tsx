60682c76800665626eb43a1e960da3b8ce08e8b4
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-05 17:12:21 -0300

    Update RequireAuth.tsx

--- src/routes/components/RequireAuth.tsx
+++ src/routes/components/RequireAuth.tsx
@@ -8,21 +8,19 @@ const RequireAuth: FC<PropsMiddlewaresAutentication> = ({ children }) => {
 	useEffect(() => {
 		if (!localStorage.getItem('token')) return navigate('/login');
 
-		if (localStorage.getItem('token')) {
-			fetch(`${import.meta.env.VITE_URL_CONECT_BACKEND}api/user/validate`, {
-				method: 'GET',
-				headers: {
-					'x-token': localStorage.getItem('token') || ''
+		fetch(`${import.meta.env.VITE_URL_CONECT_BACKEND}api/user/validate`, {
+			method: 'GET',
+			headers: {
+				'x-token': localStorage.getItem('token') || ''
+			}
+		})
+			.then(res => res.json())
+			.then(data => {
+				if (data.status == 'Error') {
+					localStorage.clear();
+					return navigate('/login');
 				}
-			})
-				.then(res => res.json())
-				.then(data => {
-					if (data.status == 'Error') {
-						localStorage.clear();
-						return navigate('/login');
-					}
-				});
-		}
+			});
 	}, [location.pathname]);
 	return (
 		<div style={{ backgroundColor: '#16191C', height: 'inherit' }}>
