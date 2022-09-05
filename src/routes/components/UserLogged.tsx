d8534b5f3344c690b8a9b708c147afcd9d04ec61
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-05 17:11:07 -0300

    enhanced security

--- src/routes/components/UserLogged.tsx
+++ src/routes/components/UserLogged.tsx
@@ -1,11 +1,35 @@
-import { FC } from 'react';
-import { Navigate } from 'react-router-dom';
+import { FC, useEffect } from 'react';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { PropsMiddlewaresAutentication } from '../types';
 
 const UserLogged: FC<PropsMiddlewaresAutentication> = ({ children }) => {
-	if (localStorage.getItem('token'))
-		return <Navigate to='/home/' replace={true} />;
-	return <div style={{ backgroundColor: '#16191C' }}>{children}</div>;
+	const navigate = useNavigate();
+	const location = useLocation();
+	useEffect(() => {
+		if (localStorage.getItem('token')) {
+			fetch(`${import.meta.env.VITE_URL_CONECT_BACKEND}api/user/validate`, {
+				method: 'GET',
+				headers: {
+					'x-token': localStorage.getItem('token') || ''
+				}
+			})
+				.then(res => res.json())
+				.then(data => {
+					if (data.status == 'Error') {
+						localStorage.clear();
+						return navigate('/login');
+					} else {
+						navigate('/home');
+					}
+				});
+		}
+	}, [location.pathname]);
+
+	return (
+		<div style={{ backgroundColor: '#16191C', height: 'inherit' }}>
+			{children}
+		</div>
+	);
 };
 
 export default UserLogged;
