3e686176d4c83ebd36907d9fe8baf81d47601be1
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-06 16:02:21 -0300

    Update SkeletonCards.tsx

--- src/pages/dashboard/components/SkeletonCards.tsx
+++ src/pages/dashboard/components/SkeletonCards.tsx
@@ -1,26 +1,29 @@
-import { Flex, Skeleton } from '@chakra-ui/react';
+import { Box, Flex, Skeleton } from '@chakra-ui/react';
 import theme from '../../../styles/theme';
 const SkeletonCards = () => {
 	const arr = ['s', 's', 's', 's', 's', 's', 's', 's', 's', 's'];
 
 	return (
-		<Flex
-			justifyContent={'center'}
-			alignContent={'center'}
-			flexWrap={'wrap'}
-			p={'0.5em'}
-			gap={4}
-			marginBottom={'55px'}
-		>
-			{arr.map((x, i) => (
-				<Skeleton
-					startColor={theme.colors.primaryBGLight}
-					endColor={theme.colors.primaryBGShade}
-					height='130px'
-					width='130px'
-				/>
-			))}
-		</Flex>
+		<Box overflowY={'scroll'}>
+			<Flex
+				justifyContent={'center'}
+				alignContent={'center'}
+				flexWrap={'wrap'}
+				p={'0.5em'}
+				gap={4}
+				marginBottom={'55px'}
+			>
+				{arr.map((x, i) => (
+					<Skeleton
+						startColor={theme.colors.primaryBGLight}
+						endColor={theme.colors.primaryBGShade}
+						height='130px'
+						width='130px'
+						borderRadius={'10px'}
+					/>
+				))}
+			</Flex>
+		</Box>
 	);
 };
 
