64a1f73a3c8019ce8850409abe562b4b1b91a820
German Hornus <hornusgerman@gmail.com>
2022-09-06 07:29:54 -0500

    skeleton and pagination

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,29 +1,86 @@
-import { Avatar, Box, Button, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, Heading, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 import CircleProgressBar from './components/CircleProgressBar/CIrcleProgressBar';
 import MobileNavBar from './components/MobileNavBar';
-import { cardData } from './components/quiz-card/data';
 import QuizCardList from './components/quiz-card/QuizCardList';
+import SkeletonCards from './components/SkeletonCards';
+import toastMaxPage from './components/toastMaxPage';
 import toastLogout from './utils/toastLogout';
 
+// function SkeletonCards() {
+// 	return <h1>This is a Skeleton</h1>;
+// }
 const Dashboard = () => {
-	const [urlAvatar, seturlAvatar] = useState(
+	const [urlAvatar, setUrlAvatar] = useState(
 		'https://avatars.githubusercontent.com/u/63567962?s=96&v=4'
 	);
+	const [page, setPage] = useState(1);
+	const [cardData, setCardData] = useState(null);
+	const [skeletonCardData, setSkeletonCardData] = useState(true);
 	const username = localStorage.getItem('username');
 	const navigate = useNavigate();
+
+	console.log({ page });
 	function handleLogout() {
 		toastLogout();
 		localStorage.removeItem('token');
 		localStorage.removeItem('username');
 		navigate('/');
 	}
+
+	function handleChangePage(e: any) {
+		// console.log(e.target.name);
+		if (e.target.name === 'add') {
+			setPage(page + 1);
+		} else if (e.target.name === 'rest') {
+			if (page === 1) {
+				return;
+			} else {
+				setPage(page - 1);
+			}
+		} else return;
+	}
+
 	// useEffect(() => {
 	// 	fetch(
 	// 		`https://boring-avatars-api.vercel.app/api/avatar?size=40&variant=marble`
 	// 	).then(e => console.log(e));
 	// }, []);
+	
+	async function fetchData({ setCardData, setSkeletonCardData }) {
+		try {
+			const response = await fetch(
+				`${import.meta.env.VITE_URL_CONECT_BACKEND}api/quiz/list?page=${page}`,
+				{
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						'x-token': localStorage.getItem('token') || '2'
+					}
+				}
+			);
+			const data = await response.json();
+			if (response.ok) {
+				setCardData(data.result);
+				setSkeletonCardData(false);
+			} else {
+				setSkeletonCardData(false);
+				throw new Error(data?.ErrorMessage);
+			}
+		} catch (error) {
+			setSkeletonCardData(false);
+			setPage(page - 1);
+			toastMaxPage();
+			console.log(error);
+		}
+	}
+	useEffect(() => {
+		setCardData(null);
+		fetchData({ setCardData, setSkeletonCardData });
+		return () => setSkeletonCardData(true);
+	}, [page]);
 
 	return (
 		<>
@@ -68,8 +125,16 @@ const Dashboard = () => {
 							title={'Jonin'}
 						/>
 					</Flex>
+					<Button name='add' onClick={handleChangePage}>
+						Pagina siguiente
+					</Button>
+					<Button name='rest' onClick={handleChangePage} disabled={page === 1}>
+						Pagina Anterior
+					</Button>
+					<Heading color={'#fff'}> Page: {page}</Heading>
 				</Flex>
-				<QuizCardList QuizCards={cardData} />
+				{cardData && <QuizCardList QuizCards={cardData} />}
+				{skeletonCardData && <SkeletonCards />}
 				<Box display={{ md: 'none' }}>
 					<MobileNavBar />
 				</Box>
