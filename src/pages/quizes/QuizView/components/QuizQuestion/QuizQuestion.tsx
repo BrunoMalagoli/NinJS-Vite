424bf7dc174ada318164473238b63085c0c16879
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-08 17:07:13 -0300

    Now you recieve the code string from the backend

--- src/pages/quizes/QuizView/components/QuizQuestion/QuizQuestion.tsx
+++ src/pages/quizes/QuizView/components/QuizQuestion/QuizQuestion.tsx
@@ -5,10 +5,6 @@ import Answers from './components/Answers';
 import QuizCode from './components/QuizCode';
 const QuizQuestion = ({ quizData }: qData) => {
 	console.log({ quizData });
-	quizData.code = `const func = (firstParam, secondParam = 4){
-		return firstParam + secondParam 
-	  }
-	  console.log(func(2,null))`;
 	return (
 		<Box>
 			<QuizCode code={quizData.code} />
