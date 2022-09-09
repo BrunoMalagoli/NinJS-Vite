3bb1c387dec026dbd6d303c1eac764da172a2d86
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-09 14:25:36 -0300

    Rename Settings Page to Profile Page

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,12 +1,12 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
-const SettingsPage = () => {
+const ProfilePage = () => {
 	return (
 		<Flex h='100vh'>
-			<h5 style={{ color: 'white' }}>SettingsPage</h5>
+			<h5 style={{ color: 'white' }}>ProfilePage</h5>
 		</Flex>
 	);
 };
 
-export default SettingsPage;
+export default ProfilePage;
