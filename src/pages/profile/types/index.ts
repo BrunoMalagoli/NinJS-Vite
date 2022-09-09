c674761ff2c763acf8689b564371b58571ea7ef9
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-09 19:47:12 -0300

    Created Username input
    - It internally has the avatars

--- src/pages/profile/types/index.ts
+++ src/pages/profile/types/index.ts
@@ -1,5 +1,9 @@
 import { UseRadioProps } from '@chakra-ui/react';
 
+export type ProfileInfo = {
+	username: string;
+};
+
 export type AvatarRadioCardProps = {
 	variant: Variants;
 	props: UseRadioProps;
