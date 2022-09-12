f7ac76263ea94dffaf57bf7edba1b45d930a03a3
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-12 17:59:12 -0300

    Update schema.ts

--- src/pages/profile/validation/schema.ts
+++ src/pages/profile/validation/schema.ts
@@ -3,7 +3,6 @@ import * as yup from 'yup';
 const profileSchema = yup.object({
 	username: yup
 		.string()
-		.required('Campo requerido')
 		.min(4, 'El nombre de usuario debe tener al menos 4 caracteres')
 		.max(20, 'El nombre de usuario puede tener hasta 20 caracteres')
 });
