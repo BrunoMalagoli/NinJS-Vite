9af32a6226fb58b66a2b89f10a1fffb04db365bb
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-12 22:40:48 -0300

    Now the context is at high level

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import DataContextProvider from './context/data/DataContextProvider';
+import ProfileContextProvider from './context/profile/ProfileContextProvider';
 import { AppRoutes } from './routes/AppRouter';
 import theme from './styles/theme';
 
@@ -8,8 +10,14 @@ function App() {
 	return (
 		<ChakraProvider theme={theme}>
 			<BrowserRouter>
-				<ToastContainer />
-				<AppRoutes />
+				<DataContextProvider>
+					<ProfileContextProvider>
+						<>
+							<ToastContainer />
+							<AppRoutes />
+						</>
+					</ProfileContextProvider>
+				</DataContextProvider>
 			</BrowserRouter>
 		</ChakraProvider>
 	);
