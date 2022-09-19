0c623e75b770775297537a433149735d1258864e
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-19 14:23:54 -0300

    created global styles for toastify

---
+++ src/styles/toast.ts
@@ -0,0 +1,9 @@
+import { CSSProperties } from 'react';
+import theme from './theme';
+
+const toastStyles: CSSProperties = {
+	backgroundColor: theme.colors.primaryBGShade,
+	color: 'white'
+};
+
+export default toastStyles;
