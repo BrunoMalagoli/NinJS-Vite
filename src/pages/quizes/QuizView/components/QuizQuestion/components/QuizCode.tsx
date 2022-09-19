41bdf07ba6722367ebe7cc54731b61b26ade104f
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-19 13:52:20 -0300

    Fixed styles in QuizCode

--- src/pages/quizes/QuizView/components/QuizQuestion/components/QuizCode.tsx
+++ src/pages/quizes/QuizView/components/QuizQuestion/components/QuizCode.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@chakra-ui/react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
 import styles from '../components/styles/QuizCode.module.css';
+
 const QuizCode = ({ code }: { code: string }) => {
 	return (
-		<Box height={'15vh'} className={styles.codeContainer}>
+		<Box height={'100%'} className={styles.codeContainer}>
 			<SyntaxHighlighter
 				customStyle={{ padding: '20px' }}
 				language='javascript'
