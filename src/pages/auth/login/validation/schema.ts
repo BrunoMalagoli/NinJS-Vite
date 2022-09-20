7701230c80d10c7d1a60ad2147f1b078b4106026
German Hornus <hornusgerman@gmail.com>
2022-09-20 15:02:31 -0500

    change language of error validation in login and register

--- src/pages/auth/login/validation/schema.ts
+++ src/pages/auth/login/validation/schema.ts
@@ -2,14 +2,14 @@ import * as yup from 'yup';
 export const userLoginSchema = yup.object({
 	email: yup
 		.string()
-		.required('Email is required')
-		.email('Email should be valid'),
+		.required('El email es requerido')
+		.email('El email debe ser válido'),
 	password: yup
 		.string()
-		.min(8, 'Password should have at least 8 characters')
-		.required('Password is required')
+		.min(8, 'La contraseña debe tener al menos 8 caracteres')
+		.required('La contraseña es requerida')
 		.matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])./, {
 			message:
-				'Password should have at least 1 number, 1 Capital letter, 1 lower case letter and 8 characters'
+				'La contraseña debe tener al menos 1 número, 1 mayúscula y 1 minúscula'
 		})
 });
--- src/pages/auth/register/validation/schema.ts
+++ src/pages/auth/register/validation/schema.ts
@@ -2,21 +2,21 @@ import * as yup from 'yup';
 export const userRegisterSchema = yup.object({
 	username: yup
 		.string()
-		.required('Username is required')
-		.min(4, 'Username should have at least 4 characters'),
+		.required('Username es requerido')
+		.min(4, 'Username debe tener al menos 4 carácteres'),
 	email: yup
 		.string()
-		.required('Email is required')
-		.email('Email should be valid'),
+		.required('Email es requerido')
+		.email('El email debe ser válido'),
 	password: yup
 		.string()
-		.min(8, 'Password should have at least 8 characters')
-		.required('Password is required')
+		.min(8, 'La contraseña debe tener al menos 8 carácteres')
+		.required('La contraseña es requerida')
 		.matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])./, {
 			message:
-				'Password should have at least 1 number, 1 Capital letter, 1 lower case letter and 8 characters'
+				'La contraseña debe tener al menos 1 número, 1 mayúscula y 1 minúscula'
 		}),
 	repeatPassword: yup
 		.string()
-		.oneOf([yup.ref('password'), null], 'Passwords must match')
+		.oneOf([yup.ref('password'), null], 'Las contraseñas deben coincidir')
 });
