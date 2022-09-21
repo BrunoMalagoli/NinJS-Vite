3416c80ba2666096107700d93faf716e1b81ea9f
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-21 20:08:06 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import AnswersContextProvider from './context/answers/AnswersContextProvider';
-import DataContextProvider from './context/data/DataContextProvider';
-import ProfileContextProvider from './context/profile/ProfileContextProvider';
 import { AppRoutes } from './routes/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ToastContainer } from 'react-toastify';
 import theme from './styles/theme';
-import 'react-toastify/dist/ReactToastify.css';
+import DataContextProvider from './context/data/DataContextProvider';
 import NavBarContextProvider from './context/navBar/NavBarContextProvider';
+import AnswersContextProvider from './context/answers/AnswersContextProvider';
+import ProfileContextProvider from './context/profile/ProfileContextProvider';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 	return (
