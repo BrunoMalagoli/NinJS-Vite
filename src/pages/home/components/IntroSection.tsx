900c26653c42ee07a28138a29eb2735315487ca0
Bruno Malagoli <bmalagoli2@gmail.com>
2022-09-21 00:05:33 -0300

    Intro section implementation

--- src/pages/home/components/IntroSection.tsx
+++ src/pages/home/components/IntroSection.tsx
@@ -1,13 +1,13 @@
 import { Container, Flex, Heading, Image, Text } from '@chakra-ui/react';
-import React from 'react';
+import styles from './IntroSection.module.css';
 
 const IntroSection = () => {
 	return (
 		<Container
-			bgColor={'primaryYellow'}
+			bgColor={'#16191C'}
 			padding={'0'}
-			height={'60vh'}
-			minWidth={'100%'}
+			height={'45vh'}
+			maxWidth={'100%'}
 		>
 			<Flex
 				height={'100%'}
@@ -16,17 +16,29 @@ const IntroSection = () => {
 				alignItems={'center'}
 				wrap={'wrap'}
 			>
-				<Heading textAlign={'center'} width={'100%'}>
-					Conviertete en un verdadero ninja...
+				<Heading
+					fontSize={'3xl'}
+					color={'primaryYellow'}
+					textAlign={'center'}
+					width={'100%'}
+				>
+					Forja tu propio camino ninja...
 				</Heading>
+				<Image className={styles.cornerRight} src='/temple-corner.svg' />
+				<Image className={styles.cornerLeft} src='/temple-corner.svg' />
 				<Image
 					width={'100%'}
 					height={'40%'}
 					src='/ninja.svg'
 					alt='Ninja mask'
 				/>
-				<Text fontSize={'xl'} fontWeight={'bold'} textAlign={'center'}>
-					...y haz tu camino hacia lo mas alto
+				<Text
+					color={'primaryYellow'}
+					fontSize={'xl'}
+					fontWeight={'bold'}
+					textAlign={'center'}
+				>
+					...y escala hasta lo mas alto del ranking
 				</Text>
 			</Flex>
 		</Container>
