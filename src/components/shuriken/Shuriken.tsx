1ca20735f948edbcd26f1e1351eb2f0fb4bbeb59
Bruno Malagoli <bmalagoli2@gmail.com>
2022-09-23 13:44:48 -0300

    Import fixes

--- src/components/shuriken/Shuriken.tsx
+++ src/components/shuriken/Shuriken.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from 'react';
 import { ShurikenProps } from './types';
 import './Shuriken.styles.css';
+
 /**
  * @description Set size to 100 if you want it to rotate and not break the page
  * @param rotate Sets if it will rotate or not
