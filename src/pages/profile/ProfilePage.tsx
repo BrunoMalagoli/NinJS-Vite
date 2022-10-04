052cb65e7ac8c72e52d95c893f0e6785649a43ef
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-04 18:16:34 -0300

    Hot Fix

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -27,20 +27,28 @@ const ProfilePage = () => {
 			>
 				<Avatar name={username} variant={variant} size={'1em'} />
 				<Flex
-					direction='column'
-					flexGrow={1}
-					pl='24px'
-					justifyContent={'center'}
+					direction={'row'}
+					py='16px'
+					borderRadius={'0.5em'}
+					color={'white'}
+					w='95%'
+					fontSize={{ base: '80px', md: '100px' }}
 				>
-					<Text color={'white'} fontSize={{ base: 'md', md: 'xl' }}>
-						{username}
-					</Text>
-					<Text color={'white'} fontSize={{ base: 'xs', sm: 'md', md: 'xl' }}>
-						Completadas: {completed} / 48
-					</Text>
+					<Avatar name={username} variant={variant} size={'1em'} />
+					<Flex
+						direction='column'
+						flexGrow={1}
+						pl='24px'
+						justifyContent={'center'}
+					>
+						<Text color={'white'} fontSize={{ base: 'md', md: 'xl' }}>
+							{username}
+						</Text>
+						<Text color={'white'} fontSize={{ base: 'xs', sm: 'md', md: 'xl' }}>
+							Completadas: {completed} / 48
+						</Text>
+					</Flex>
 				</Flex>
-			</Flex>
-			<Flex flexDirection={'column'}>
 				<Flex
 					justifyContent={'space-between'}
 					width='100%'
