20daf9e340a4ceda74a168721efe028a9832dfea
Jalinson Diaz <zjaaaldev@gmail.com>
2022-10-04 17:48:33 -0300

    Enhanced design on cards

--- src/pages/dashboard/components/quiz-card/QuizCard.tsx
+++ src/pages/dashboard/components/quiz-card/QuizCard.tsx
@@ -1,5 +1,6 @@
-import { Badge, Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { FC } from 'react';
+import { HiCheck, HiX } from 'react-icons/hi';
 
 import { useNavigate } from 'react-router-dom';
 import theme from '../../../../styles/theme';
@@ -12,7 +13,6 @@ const QuizCard: FC<QuizCardProps> = ({
 	number,
 	completed
 }) => {
-	const Background = BackgroundSwitch[difficult];
 	const navigate = useNavigate();
 	const handleClick = () => {
 		let category = questionID.match(/[A-Z]/g)![0];
@@ -23,7 +23,7 @@ const QuizCard: FC<QuizCardProps> = ({
 			onClick={handleClick}
 			className={`${styles['glass-card-' + difficult]} ${styles.card}`}
 			_hover={{
-				background: [theme.colors.primaryBGLightHover],
+				background: [theme.colors.primaryBGShade],
 				transform: 'scale(1.03)'
 			}}
 			w={{
@@ -37,7 +37,6 @@ const QuizCard: FC<QuizCardProps> = ({
 				xl: '170px'
 			}}
 		>
-			<Background />
 			<Box>
 				<Text fontSize={{ base: '14px', sm: '16px' }}>{number}</Text>
 			</Box>
@@ -52,15 +51,10 @@ const QuizCard: FC<QuizCardProps> = ({
 			<Flex justifyContent={'end'}>
 				{typeof completed == 'undefined' ? (
 					<Box height={'18px'}>{/*kind of hacky but it does the trick*/}</Box>
+				) : completed ? (
+					<HiCheck color='#25292d' size={'20px'} />
 				) : (
-					<Badge
-						fontSize={{ base: '10px', sm: '12px' }}
-						colorScheme={completed ? 'green' : 'red'}
-						variant={'subtle'}
-						borderRadius={'10px'}
-					>
-						{completed ? 'Aprobada' : 'fallada'}
-					</Badge>
+					<HiX color='#25292d' size={'20px'} />
 				)}
 			</Flex>
 		</Box>
